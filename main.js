b6c3f55bc3a4bec019df6471b7d6d69508c700db
Salepate <salepate@gmail.com>
2018-09-11 09:18:44 +0200

    rofetcher 0.0.3

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const client = require('./client.js');
 const clientdb = require('./clientdb.js');
 const dbreader = require('./dbreader.js');
 const chain = require('./chaincommand.js');
+const package = require('./package.json');
 
+
+chain.addCommand((_, cb) => { console.log(`: ro fetcher ${package.version} :`); cb(); });
 chain.addCommand(clientdb.initialize);
 chain.addCommand(dbreader.initialize);
 chain.addCommand(client.initialize);
