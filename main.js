63ef4011f95e84db1f912df4139fa2ca69e26b78
Salepate <salepate@gmail.com>
2018-09-15 09:07:40 +0200

    complete rework of bot api
    command splitted into multiple files
    mastered a bit more the nodejs way of doing weird stuff
    static content isolated from mongodb
    better api for new commands

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-const Discord = require('discord.io');
-const auth = require('./auth.json');
-const client = require('./client.js');
-const clientdb = require('./clientdb.js');
-const dbreader = require('./dbreader.js');
-const chain = require('./chaincommand.js');
+const bot = require('./lib/bot.js');
 
-chain.addCommand(clientdb.initialize);
-chain.addCommand(dbreader.initialize);
-chain.addCommand(client.initialize);
-
-chain.execute((err)=>
-{
-    console.log('bot started successfully');
-});
+bot.startBot();
